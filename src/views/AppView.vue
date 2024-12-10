6720fc1298db058afaa583ebbc8beda9c37435f8
Chris <chris@linguine.tech>
2024-12-10 16:51:54 +0100

    Add prettty header bar

--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -5,10 +5,11 @@ import LeftSideNav from '@/views/LeftSideNav.vue'
 
 <template>
   <div class="flex">
-    <div class="w-72 h-screen px-2 py-4">
+    <div class="bg-blue-800 h-80 w-full absolute top-0 left-0 right-0 z-0"></div>
+    <div class="w-72 h-screen px-2 py-4 z-10">
       <LeftSideNav class="h-full bg-white rounded-3xl" />
     </div>
-    <main class="grow">
+    <main class="grow z-10 px-8 py-4">
       <RouterView />
     </main>
   </div>
