8f05be86301cf06bd02927df0ebeec67cb66089d
Chris <chris@linguine.tech>
2024-12-11 16:28:16 +0100

    Add assessment plans view

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,6 +19,14 @@ const authenticatedRoutes = [
     component: () => import('../views/DashboardView.vue'),
   },
   {
+    path: '/assessment-plan/:id',
+    name: 'assessment-plan',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AssessmentPlanView.vue'),
+  },
+  {
     path: '/about',
     name: 'about',
     component: () => import('../views/AboutView.vue'),
--- src/views/AssessmentPlanView.vue
+++ src/views/AssessmentPlanView.vue
@@ -98,124 +98,12 @@
   </div>
   <div class="px-2 mt-8">
     <div class="bg-white h-72 rounded shadow px-4 py-2">
-      <h3 class="text-lg font-semibold text-zinc-600 mb-4">Assessment plans</h3>
-      <RouterLink
-        class="flex border-t items-center hover:bg-zinc-100"
-        :to="{ 'name': 'assessment-plan', 'params': {'id' : assessment.id}}"
-        v-for="assessment in assessmentPlans"
-        :key="assessment.id"
-      >
-        <div class="px-2 py-2">{{ assessment.id }}</div>
-        <div class="px-2 py-2 flex-1">{{ assessment.name }}</div>
-        <div class="px-2 w-1/5 h-8">
-          <LineChart
-            :options="{
-              plugins: {
-                tooltip: {
-                  enabled: false,
-                },
-              },
-              elements: {
-                point: {
-                  hoverRadius: 0,
-                  hitRadius: 0,
-                },
-              },
-            }"
-            :data="assessment.data"
-          />
-        </div>
-      </RouterLink>
+      <h3 class="text-lg font-semibold text-zinc-600 mb-4">Assessment plan: {{ this.$route.params.id }}</h3>
+      Details
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import LineChart from '@/components/charts/LineChart.vue'
-import { ref } from 'vue'
 import BarChart from '@/components/charts/BarChart.vue'
-
-const gradientConfiguration = {
-  backgroundColor: {
-    axis: 'y',
-    colors: {
-      100: 'rgba(20,184,166, .7)',
-      // 35: 'rgba(20,184,166, .7)',
-      10: 'rgba(20,184,166, .3)',
-      0: 'rgba(20,184,166, 0.0)',
-    },
-  },
-}
-
-const assessmentPlans = ref([
-  {
-    id: '848DE83F-5D02-49E3-A7D4-F6612486CC5C',
-    name: 'Public facing Kubernetes host SSH Security',
-    data: {
-      labels: ['12:00', '13:00', '14:00', '15:00', '14:00', '15:00', '16:00'],
-      datasets: [
-        {
-          gradient: gradientConfiguration,
-          data: [0, 20, 10, 8, 4, 10, 0],
-          borderColor: 'rgba(20,184,166, 0.2)',
-        },
-      ],
-    },
-  },
-  {
-    id: 'AAE3A4A9-C3F0-44B6-86FD-6ACB03B84E62',
-    name: 'Public facing Kubernetes Firewall Security',
-    data: {
-      labels: ['12:00', '13:00', '14:00', '15:00', '14:00', '15:00', '16:00'],
-      datasets: [
-        {
-          gradient: gradientConfiguration,
-          data: [0, 20, 10, 8, 4, 10, 0],
-          borderColor: 'rgba(20,184,166, 0.2)',
-        },
-      ],
-    },
-  },
-  {
-    id: 'C3ECD77D-ED33-48CA-A674-78AF707223EE',
-    name: 'Postgres access security',
-    data: {
-      labels: ['12:00', '13:00', '14:00', '15:00', '14:00', '15:00', '16:00'],
-      datasets: [
-        {
-          gradient: gradientConfiguration,
-          data: [0, 10, 15, 8, 15, 10, 15],
-          borderColor: 'rgba(20,184,166, 0.2)',
-        },
-        {
-          gradient: {
-            backgroundColor: {
-              axis: 'y',
-              colors: {
-                100: 'rgba(184,69,20,0.4)',
-                10: 'rgba(184,69,20, .3)',
-                0: 'rgba(184,69,20, 0.0)',
-              },
-            },
-          },
-          data: [0, 25, 10, 10, 25, 5, 8],
-          borderColor: 'rgba(184,69,20, 0.2)',
-        },
-      ],
-    },
-  },
-  {
-    id: 'C3ECD77D-F9F3-4D96-9740-3BAE64E4998E',
-    name: 'Azure VM Configuration - Tags and Security Groups',
-    data: {
-      labels: ['12:00', '13:00', '14:00', '15:00', '14:00', '15:00', '16:00'],
-      datasets: [
-        {
-          gradient: gradientConfiguration,
-          data: [0, 10, 20, 0, 30, 0, 25],
-          borderColor: 'rgba(20,184,166, 0.2)',
-        },
-      ],
-    },
-  },
-])
 </script>
