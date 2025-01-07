294f1741cd6a401817f77334216510a19d739be0
Chris <chris@linguine.tech>
2025-01-07 15:37:32 +0100

    Move fetch results to store

--- src/stores/api.ts
+++ src/stores/api.ts
@@ -21,6 +21,11 @@ export interface Finding {
   remarks: string
 }
 
+export interface Log {
+  title: string
+  description: string
+}
+
 export interface Result {
   _id: string
   id: string
@@ -30,6 +35,7 @@ export interface Result {
   streamId: string
   observations: Observation[]
   findings: Finding[]
+  assessmentLogEntries: Log[]
 }
 
 export interface DataResponse<T> {
@@ -40,36 +46,42 @@ export const useApiStore = defineStore('api', () => {
   const configStore = useConfigStore()
 
   async function createPlan(plan: Plan): Promise<Plan> {
-    const config = await configStore.getConfig();
+    const config = await configStore.getConfig()
     const response = await fetch(`${config.API_URL}/api/plan`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(plan),
-    });
+    })
 
     if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`);
+      throw new Error(`Error: ${response.statusText}`)
     }
 
-    const data = await response.json() as Plan;
+    const data = (await response.json()) as Plan
     return {
       ...plan,
       ...data,
-    } as Plan;
+    } as Plan
   }
 
   async function getPlan(id: string): Promise<Plan> {
     const config = await configStore.getConfig()
     const response = await fetch(`${config.API_URL}/api/plan/${id}`)
-    return await response.json() as Plan
+    return (await response.json()) as Plan
   }
 
   async function getPlans(): Promise<Plan[]> {
     const config = await configStore.getConfig()
     const response = await fetch(`${config.API_URL}/api/plans`)
-    return await response.json() as Plan[]
+    return (await response.json()) as Plan[]
+  }
+
+  async function getResult(id: string): Promise<DataResponse<Result>> {
+    const config = await configStore.getConfig()
+    const response = await fetch(`${config.API_URL}/api/results/${id}`)
+    return await response.json() as DataResponse<Result>
   }
 
   async function getPlanResults(id: string): Promise<DataResponse<Result[]>> {
@@ -82,14 +94,15 @@ export const useApiStore = defineStore('api', () => {
   async function getStreamResults(streamId: string): Promise<DataResponse<Result[]>> {
     const config = await configStore.getConfig()
     const response = await fetch(`${config.API_URL}/api/results/stream/${streamId}`)
-    return await response.json() as DataResponse<Result[]>
+    return (await response.json()) as DataResponse<Result[]>
   }
 
   return {
     getPlan,
+    getPlans,
+    createPlan,
+    getResult,
     getPlanResults,
     getStreamResults,
-    createPlan,
-    getPlans,
   }
 })
--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,16 +1,16 @@
 <template>
   <PageHeader>
-    {{ title }}
+    {{ result?.title }}
   </PageHeader>
   <PageSubHeader>
-    {{ id }}
+    {{ result?._id }}
   </PageSubHeader>
   <div class="grid grid-cols-2 gap-4 mt-4">
     <PageCard>
       <h3 class="text-lg pl-4 mb-4">Observations</h3>
       <div>
         <div
-          v-for="observation in observations"
+          v-for="observation in result?.observations"
           :key="observation.id"
           class="px-4 py-2 hover:bg-zinc-100"
         >
@@ -22,7 +22,7 @@
     <PageCard>
       <h3 class="text-lg pl-4 mb-4">Findings</h3>
       <div>
-        <div v-for="finding in findings" :key="finding.id" class="px-4 py-2 hover:bg-zinc-100">
+        <div v-for="finding in result?.findings" :key="finding.id" class="px-4 py-2 hover:bg-zinc-100">
           <p class="font-semibold">{{ finding.title }}</p>
           <p>{{ finding.description }}</p>
         </div>
@@ -31,7 +31,7 @@
     <PageCard>
       <h3 class="text-lg pl-4 mb-4">Logs</h3>
       <div>
-        <div v-for="log in logs" :key="log.id" class="px-4 py-2 hover:bg-zinc-100">
+        <div v-for="log in result?.assessmentLogEntries" class="px-4 py-2 hover:bg-zinc-100">
           <p class="font-semibold">{{ log.title }}</p>
           <p>{{ log.description }}</p>
         </div>
@@ -39,44 +39,23 @@
     </PageCard>
   </div>
 </template>
-<script setup>
+<script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import PageHeader from '@/components/PageHeader.vue'
 import PageCard from '@/components/PageCard.vue'
 import PageSubHeader from '@/components/PageSubHeader.vue'
-import { useConfigStore } from '@/stores/config';
+import { type Result, useApiStore } from '@/stores/api'
 
-const configStore = useConfigStore();
+const apiStore = useApiStore();
 const route = useRoute()
-const id = ref(route.params.id)
-const title = ref('')
-const findings = ref([])
-const observations = ref([])
-const logs = ref([])
-const risks = ref([])
+const id = route.params.id as string
 
-async function fetchResult() {
-  const config = await useConfigStore().getConfig();
-  return fetch(`${config.API_URL}/api/results/${id.value}`)
-    .then((response) => {
-      return response.json()
-    })
-    .then((json) => {
-      return json.data
-    })
-}
+const result = ref<Result>({} as Result)
 
 onMounted(() => {
-  fetchResult().then((result) => {
-    if (result) {
-      console.log(result)
-      title.value = result.title
-      findings.value = result.findings
-      observations.value = result.observations
-      logs.value = result.assessmentLogEntries
-      console.log(result)
-    }
+  apiStore.getResult(id).then((res) => {
+    result.value = res.data
   })
 })
 </script>
