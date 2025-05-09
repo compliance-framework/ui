487850a80e0155c24757037d8f7911375de56632
Chris <chris@linguine.tech>
2025-05-09 12:08:38 +0200

    Initial catalog editor

--- src/views/CatalogGroup.vue
+++ src/views/CatalogGroup.vue
@@ -2,7 +2,7 @@
   <CollapsableGroup>
     <template #header>
       <div class="py-4 px-4">
-        <span class="bg-slate-100 dark:bg-slate-800 border border-slate-200 dark:border-slate-800 text-gray-800 dark:text-slate-300 rounded-md text-sm px-2 py-1 mr-2">{{group.id}}</span>{{ group.title }}
+        <span class="bg-slate-100 dark:bg-slate-800 border border-slate-200 dark:border-slate-800 text-gray-800 dark:text-slate-300 rounded-md text-sm px-2 py-1 mr-2">{{group.id}}</span>{{ group.title }} <span class="text-gray-400 dark:text-slate-300 text-sm px-2 py-1">Group</span>
       </div>
     </template>
     <div class="px-4 py-4 border-b dark:border-slate-700">
@@ -25,23 +25,29 @@
         <CatalogControl v-for="control in controls" :key="control.id" :control="control" :catalog="props.catalog" />
         <CatalogGroup v-for="group in groups" :key="group.id" :group="group" :catalog="props.catalog" />
       </div>
+      <div class="mt-4">
+        <TertiaryButton v-if="controls.length == 0" @click="showGroupForm = true">Add Group</TertiaryButton>
+        <TertiaryButton v-if="groups.length == 0" @click="showControlForm = true" class="ml-2">Add Control</TertiaryButton>
+        <GroupCreateModal @created="groupCreated" :catalog="catalog" :parent="props.group" v-model="showGroupForm" />
+        <ControlCreateModal @created="controlCreated" :catalog="catalog" :parent-group="props.group" v-model="showControlForm" />
+      </div>
     </div>
   </CollapsableGroup>
 </template>
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { type Group, useGroupsStore } from '@/stores/groups.ts'
 import CollapsableGroup from '@/components/CollapsableGroup.vue'
-import { type Control, useControlStore } from '@/stores/controls.ts'
-import type { Catalog } from '@/stores/catalogs.ts'
+import { type Catalog, type Group, type Control, useCatalogStore } from '@/stores/catalogs.ts'
+import TertiaryButton from '@/components/TertiaryButton.vue'
+import GroupCreateModal from '@/components/catalogs/GroupCreateModal.vue'
+import ControlCreateModal from '@/components/catalogs/ControlCreateModal.vue'
 
 const props = defineProps<{
   catalog: Catalog,
   group: Group,
 }>()
 
-const groupStore = useGroupsStore()
-const controlStore = useControlStore()
+const catalogStore = useCatalogStore()
 const groups = ref<Group[]>([]);
 const controls = ref<Control[]>([]);
 
@@ -58,11 +64,21 @@ function getPart(type: string) {
 }
 
 onMounted(() => {
-  groupStore.children(props.catalog, props.group).then((data) => {
+  catalogStore.listGroupGroups(props.catalog.uuid, props.group).then((data) => {
     groups.value = data.data
   })
-  controlStore.group(props.catalog, props.group).then((data) => {
+  catalogStore.listGroupControls(props.catalog.uuid, props.group).then((data) => {
     controls.value = data.data
   });
 })
+
+
+const showGroupForm = ref<boolean>(false);
+const showControlForm = ref<boolean>(false);
+function groupCreated(group: Group) {
+  groups.value.push(group)
+}
+function controlCreated(control: Control) {
+  controls.value.push(control)
+}
 </script>
--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -6,21 +6,31 @@
     class="mt-4 rounded-md bg-white dark:bg-slate-900 border-collapse border dark:border-slate-700"
   >
     <CatalogGroup v-for="group in groups" :key="group.id" :group="group" :catalog="catalog" />
+    <CatalogControl v-for="control in controls" :key="control.id" :control="control" :catalog="catalog" />
+  </div>
+  <div class="mt-4">
+    <TertiaryButton v-if="controls.length == 0" @click="showGroupForm = true">Add Group</TertiaryButton>
+    <TertiaryButton v-if="groups.length == 0" @click="showControlForm = true" class="ml-2">Add Control</TertiaryButton>
+    <GroupCreateModal @created="groupCreated" :catalog="catalog" v-model="showGroupForm" />
+    <ControlCreateModal @created="controlCreated" :catalog="catalog" v-model="showControlForm" />
   </div>
   <div class="h-screen w-full"></div> <!-- A screen height div to prevent collapse scrolling back up after closing -->
 </template>
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import PageHeader from '@/components/PageHeader.vue'
-import { type Catalog, useCatalogStore } from '@/stores/catalogs.ts'
+import { type Catalog, type Group, type Control, useCatalogStore } from '@/stores/catalogs.ts'
 import { useRoute } from 'vue-router'
 import PageSubHeader from '@/components/PageSubHeader.vue'
-import { type Group, useGroupsStore } from '@/stores/groups.ts'
+import CatalogControl from '@/views/CatalogControl.vue'
+import TertiaryButton from '@/components/TertiaryButton.vue'
+import GroupCreateModal from '@/components/catalogs/GroupCreateModal.vue'
+import ControlCreateModal from '@/components/catalogs/ControlCreateModal.vue'
 
 const catalogStore = useCatalogStore()
-const groupStore = useGroupsStore()
 const catalog = ref<Catalog>({} as Catalog);
 const groups = ref<Group[]>([]);
+const controls = ref<Control[]>([]);
 
 const route = useRoute();
 const id = route.params.id as string;
@@ -28,9 +38,21 @@ const id = route.params.id as string;
 onMounted(() => {
   catalogStore.get(id).then((data) => {
     catalog.value = data.data
-    groupStore.catalog(catalog.value as Catalog).then((data) => {
+    catalogStore.listGroups(id).then((data) => {
       groups.value = data.data
+    })
+    catalogStore.listControls(id).then((data) => {
+      controls.value = data.data
     })
   })
 })
+
+const showGroupForm = ref<boolean>(false);
+const showControlForm = ref<boolean>(false);
+function groupCreated(group: Group) {
+  groups.value.push(group)
+}
+function controlCreated(control: Control) {
+  controls.value.push(control)
+}
 </script>
