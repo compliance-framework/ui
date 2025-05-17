bc2836c625104497b5866bab704352e9967722ac
Chris <chris@linguine.tech>
2025-05-17 14:14:45 +0200

    Fix type check

--- src/components/SystemCharacteristicsDiagramGroupForm.vue
+++ src/components/SystemCharacteristicsDiagramGroupForm.vue
@@ -24,12 +24,12 @@ async function updateCharacteristics() {
     <div class="grid grid-cols-12 gap-y-4 items-start">
       <label class="inline-block pt-2">Description</label>
       <div class="col-span-11">
-        <FormTextarea v-model="model.description" class="field-sizing-content" />
+        <FormTextarea v-model="model?.description" class="field-sizing-content" />
       </div>
 
       <label class="inline-block pt-2">Remarks</label>
       <div class="col-span-11">
-        <FormTextarea v-model="model.remarks" class="field-sizing-content" />
+        <FormTextarea v-model="model?.remarks" class="field-sizing-content" />
       </div>
     </div>
 
