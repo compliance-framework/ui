24c7fd172c0778c13fe2da6ebff6604bcdb3979a
Chris <chris@linguine.tech>
2025-06-16 13:43:48 +0100

    Remove keepalive for guest layout

--- src/views/layouts/Guest.vue
+++ src/views/layouts/Guest.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div class="flex">
-    <main class="z-10 px-12 py-4 bg-zinc-50 dark:bg-slate-950 text-zinc-800 dark:text-slate-50 w-full h-screen">
-        <router-view v-slot="{ Component }">
-          <KeepAlive include="CatalogView">
-            <component :is="Component" />
-          </KeepAlive>
-        </router-view>
+    <main
+      class="z-10 px-12 py-4 bg-zinc-50 dark:bg-slate-950 text-zinc-800 dark:text-slate-50 w-full h-screen"
+    >
+      <router-view v-slot="{ Component }">
+        <component :is="Component" />
+      </router-view>
     </main>
   </div>
 </template>
 
 <style>
-body, html {
+body,
+html {
   @apply bg-white;
 }
 </style>
