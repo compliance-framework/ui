5420cbbd0288f5ec0728fe75ad6b6631d5c232b4
Chris <chris@linguine.tech>
2025-07-22 08:53:13 +0100

    Remove unused class

--- src/components/navigation/SideNavLink.vue
+++ src/components/navigation/SideNavLink.vue
@@ -10,7 +10,7 @@ defineProps<{
 <template>
   <RouterLink
     :to="to"
-    class="block py-2 router-link hover:bg-gray-50 dark:hover:bg-slate-800 pl-4 border-l-transparent border-l-8 text-zinc-700 dark:text-slate-200"
+    class="block py-2 hover:bg-gray-50 dark:hover:bg-slate-800 pl-4 border-l-transparent border-l-8 text-zinc-700 dark:text-slate-200"
   >
     <slot />
   </RouterLink>
