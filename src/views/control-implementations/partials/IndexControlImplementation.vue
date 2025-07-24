b42ed024e7ee4708872055705e0bdd2907b99b43
Chris <chris@linguine.tech>
2025-07-24 16:24:39 +0100

    Add badge for bycomponent count

--- src/views/control-implementations/partials/IndexControlImplementation.vue
+++ src/views/control-implementations/partials/IndexControlImplementation.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
+import Badge from '@/volt/Badge.vue';
 import type { Control } from '@/oscal';
-import type { ImplementedRequirement } from '@/stores/system-security-plans.ts';
-import PartDisplayEditor from '@/components/PartDisplayEditor.vue';
+import type {
+  ImplementedRequirement,
+  Statement,
+} from '@/stores/system-security-plans.ts';
 import PartDisplay from '@/components/PartDisplay.vue';
 import type { Part } from '@/stores/types.ts';
-import { useToggle } from '@/composables/useToggle'
+import { ref, watchEffect } from 'vue';
 
-const props = defineProps<{
+const { control, implementation } = defineProps<{
   control: Control;
   implementation: ImplementedRequirement | undefined | null;
 }>();
+
+const statements = ref<{ [key: string]: Statement }>({});
+watchEffect(() => {
+  statements.value = {};
+  for (const statement of implementation?.statements || []) {
+    statements.value[statement.statementId] = statement;
+  }
+});
+
 
 function getLabel(part: Part): string {
   if (part.props) {
@@ -52,39 +64,58 @@ function onMouseLeave(e: MouseEvent) {
 
 function onPartSelect(e: Event, asd: Part) {
   e.preventDefault();
-  console.log('selected ' + asd.id)
+  console.log('selected ' + asd.id);
+}
+
+function findStatement(part: Part): Statement | null | undefined {
+  if (implementation) {
+    if (implementation.statements) {
+      for (const statement of implementation.statements ?? []) {
+        if (statement.statementId == part.id) {
+          return statement;
+        }
+      }
+    }
+  }
+  return;
 }
 </script>
 
 <template>
-  <div
-    class="part-display"
-  >
-      <div
-        class="pl-4 py-2 border-l-8 dark:border-l-slate-700 dark:bg-slate-800 rounded-xl "
+  <div class="part-display">
+    <div
+      class="px-4 py-2 border-l-8 dark:border-l-slate-700 dark:bg-slate-800 rounded-xl"
+    >
+      <PartDisplay
+        v-for="part in control.parts?.filter(
+          (part) => part.name == 'statement',
+        ) || []"
+        :key="part.id"
+        :part="part"
+        @mouseover="onMouseOver"
+        @mouseout="onMouseLeave"
+        @selected="onPartSelect"
       >
-        <PartDisplay
-          v-for="part in control.parts?.filter(
-            (part) => part.name == 'statement',
-          ) || []"
-          :key="part.id"
-          :part="part"
-          @mouseover="onMouseOver"
-          @mouseout="onMouseLeave"
-          @selected="onPartSelect"
-        >
-          <template #default="{ part }">
-            <p
-              v-if="getText(part)"
-              class="p-0.5"
-              @mouseover="onMouseOver"
-              @mouseout="onMouseLeave"
-            >
-              {{ getText(part) }}
-            </p>
-          </template>
-        </PartDisplay>
-      </div>
+        <template #default="{ part }">
+          <div
+            v-if="getText(part)"
+            class="p-0.5"
+            @mouseover="onMouseOver"
+            @mouseout="onMouseLeave"
+          >
+            <span>{{ getText(part) }}</span>
+            <template v-if="statements[part.id]">
+              <Badge
+                class="ml-2"
+                v-if="statements[part.id].byComponents"
+                :value="statements[part.id].byComponents?.length"
+                severity="info"
+              />
+            </template>
+          </div>
+        </template>
+      </PartDisplay>
+    </div>
     <!--    <PartDisplayEditor v-for="part in control.parts?.filter(part => part.name == 'statement') || []" :key="part.id" :part="part"></PartDisplayEditor>-->
   </div>
   {{ implementation }}
@@ -93,7 +124,7 @@ function onPartSelect(e: Event, asd: Part) {
 <style>
 @reference "@/assets/main.css";
 
-.part-display .hover  {
-  @apply bg-blue-500 cursor-pointer;
+.part-display .hover {
+  @apply dark:bg-slate-600 cursor-pointer;
 }
 </style>
