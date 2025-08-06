c9f95712d8c13e7a274d4502b5d9cd0559b2a420
Chris <chris@linguine.tech>
2025-08-06 12:37:30 +0100

    Add initial implementation

--- src/stores/evidence.ts
+++ src/stores/evidence.ts
@@ -1,36 +1,34 @@
 import { defineStore } from 'pinia';
 import { useConfigStore } from '@/stores/config.ts';
 import { type Filter } from '@/parsers/labelfilter.ts';
-import type {Activity} from '@/stores/activities.ts';
-import type {
-  DataResponse,
-  Link,
-  Property
-} from '@/stores/types.ts'
-import type { Control } from '@/stores/catalogs.ts'
+import type { Activity } from '@/stores/activities.ts';
+import type { DataResponse, Link, Property } from '@/stores/types.ts';
+import type { Control } from '@/stores/catalogs.ts';
+import type { BackMatter } from '@/oscal';
 
 export interface EvidenceStatus {
-  reason: string
-  state: string
+  reason: string;
+  state: string;
 }
 
 export interface EvidenceLabel {
-  name: string
-  value: string
+  name: string;
+  value: string;
 }
 
 export interface Evidence {
-  id: string
-  uuid: string
-  title: string
-  description?: string
-  labels: EvidenceLabel[]
+  id: string;
+  uuid: string;
+  title: string;
+  description?: string;
+  labels: EvidenceLabel[];
   start: string; // ISO 8601
   end: string; // ISO 8601
   links?: Link[];
   props?: Property[];
-  status: EvidenceStatus
-  activities: Activity[]
+  backMatter?: BackMatter;
+  status: EvidenceStatus;
+  activities: Activity[];
 }
 
 export interface ComplianceIntervalStatus {
@@ -54,11 +52,11 @@ export interface ComplianceInterval {
 // }
 
 interface ForControlMetadata {
-  control: Control
+  control: Control;
 }
 
-export interface ForControlResponse  extends DataResponse<Evidence[]> {
-  metadata: ForControlMetadata
+export interface ForControlResponse extends DataResponse<Evidence[]> {
+  metadata: ForControlMetadata;
 }
 
 export const useEvidenceStore = defineStore('evidence', () => {
@@ -72,7 +70,9 @@ export const useEvidenceStore = defineStore('evidence', () => {
 
   async function getForControl(controlId: string): Promise<ForControlResponse> {
     const config = await configStore.getConfig();
-    const response = await fetch(`${config.API_URL}/api/evidence/for-control/${controlId}`);
+    const response = await fetch(
+      `${config.API_URL}/api/evidence/for-control/${controlId}`,
+    );
     return (await response.json()) as ForControlResponse;
   }
 
@@ -95,51 +95,25 @@ export const useEvidenceStore = defineStore('evidence', () => {
     return (await response.json()) as DataResponse<Evidence[]>;
   }
 
-  // async function searchBySubject(
-  //   filter: Filter,
-  // ): Promise<DataResponse<FindingBySubject[]>> {
-  //   const config = await configStore.getConfig();
-  //   const response = await fetch(
-  //     `${config.API_URL}/api/findings/search-by-subject`,
-  //     {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         filter: filter,
-  //       }),
-  //     },
-  //   );
-  //
-  //   if (!response.ok) {
-  //     throw new Error(`Error: ${response.statusText}`);
-  //   }
-  //
-  //   return (await response.json()) as DataResponse<FindingBySubject[]>;
-  // }
-  //
-  // async function getByControlClass(
-  //   className: string,
-  // ): Promise<DataResponse<FindingsByClassName[]>> {
-  //   const config = await configStore.getConfig();
-  //   const response = await fetch(
-  //     `${config.API_URL}/api/findings/by-control/${className}`,
-  //     {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     },
-  //   );
-  //
-  //   if (!response.ok) {
-  //     throw new Error(`Error: ${response.statusText}`);
-  //   }
-  //
-  //   return (await response.json()) as DataResponse<FindingsByClassName[]>;
-  // }
-  //
+  async function create(
+    evidence: Partial<Evidence>,
+  ): Promise<DataResponse<Evidence>> {
+    const config = await configStore.getConfig();
+    const response = await fetch(`${config.API_URL}/api/evidence`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(evidence),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    return (await response.json()) as DataResponse<Evidence>;
+  }
+
   async function history(uuid: string): Promise<DataResponse<Evidence[]>> {
     const config = await configStore.getConfig();
     const response = await fetch(
@@ -165,9 +139,10 @@ export const useEvidenceStore = defineStore('evidence', () => {
   ): Promise<DataResponse<ComplianceInterval[]>> {
     const config = await configStore.getConfig();
     const response = await fetch(
-      `${config.API_URL}/api/evidence/status-over-time?` + new URLSearchParams({
-        intervals: intervals ?? "",
-      }),
+      `${config.API_URL}/api/evidence/status-over-time?` +
+        new URLSearchParams({
+          intervals: intervals ?? '',
+        }),
       {
         method: 'POST',
         headers: {
@@ -192,9 +167,10 @@ export const useEvidenceStore = defineStore('evidence', () => {
   ): Promise<DataResponse<ComplianceInterval[]>> {
     const config = await configStore.getConfig();
     const response = await fetch(
-      `${config.API_URL}/api/evidence/status-over-time/${uuid}?` + new URLSearchParams({
-        intervals: intervals ?? "",
-      }),
+      `${config.API_URL}/api/evidence/status-over-time/${uuid}?` +
+        new URLSearchParams({
+          intervals: intervals ?? '',
+        }),
       {
         method: 'GET',
         headers: {
@@ -210,64 +186,24 @@ export const useEvidenceStore = defineStore('evidence', () => {
     return (await response.json()) as DataResponse<ComplianceInterval[]>;
   }
 
-  async function getComplianceForControl(control: Control): Promise<DataResponse<ComplianceIntervalStatus[]>> {
-    const config = await configStore.getConfig()
-    const response = await fetch(`${config.API_URL}/api/evidence/compliance-by-control/${control.id}`)
-    return (await response.json()) as DataResponse<ComplianceIntervalStatus[]>
+  async function getComplianceForControl(
+    control: Control,
+  ): Promise<DataResponse<ComplianceIntervalStatus[]>> {
+    const config = await configStore.getConfig();
+    const response = await fetch(
+      `${config.API_URL}/api/evidence/compliance-by-control/${control.id}`,
+    );
+    return (await response.json()) as DataResponse<ComplianceIntervalStatus[]>;
   }
 
-  //
-  // async function getComplianceForControl(
-  //   _class: string,
-  //   id: string,
-  // ): Promise<DataResponse<ComplianceIntervalStatus[]>> {
-  //   const config = await configStore.getConfig();
-  //   const response = await fetch(
-  //     `${config.API_URL}/api/findings/instant-compliance-by-control/${_class}/${id}`,
-  //     {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     },
-  //   );
-  //
-  //   if (!response.ok) {
-  //     throw new Error(`Error: ${response.statusText}`);
-  //   }
-  //   return (await response.json()) as DataResponse<ComplianceIntervalStatus[]>;
-  // }
-  //
-  // async function getAllControlClasses(): Promise<DataResponse<string[]>> {
-  //   const config = await configStore.getConfig();
-  //   const response = await fetch(
-  //     `${config.API_URL}/api/findings/list-control-classes`,
-  //     {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     },
-  //   );
-  //
-  //   if (!response.ok) {
-  //     throw new Error(`Error: ${response.statusText}`);
-  //   }
-  //   return (await response.json()) as DataResponse<string[]>;
-  // }
-
   return {
-    get: get,
+    get,
     getForControl,
-    search: search,
-    // searchBySubject,
-    history: history,
+    search,
+    create,
+    history,
     getComplianceForSearch,
     getComplianceForUUID,
     getComplianceForControl,
-    // getByControlClass,
-    // getAllControlClasses,
-    // getComplianceForControl,
-    // searchForControlID,
   };
 });
--- src/views/evidence/CreateView.vue
+++ src/views/evidence/CreateView.vue
@@ -3,19 +3,59 @@
   <PageSubHeader>
     Create a new piece of evidence to support your control implementations.
   </PageSubHeader>
-  <div>Here we are</div>
+  <form @submit.prevent="submit">
+    <div>
+      <label for="title">Title</label>
+      <input id="title" v-model="evidence.title" required />
+    </div>
+    <div>
+      <label for="description">Description</label>
+      <textarea id="description" v-model="evidence.description" />
+    </div>
+    <div>
+      <label for="start">Start Date</label>
+      <input
+        id="start"
+        type="datetime-local"
+        v-model="evidence.start"
+        required
+      />
+    </div>
+    <div>
+      <label for="end">End Date</label>
+      <input id="end" type="datetime-local" v-model="evidence.end" required />
+    </div>
+    <div>
+      <label for="status">Status</label>
+      <input id="status" v-model="evidence.status.state" required />
+    </div>
+    <div>
+      <label for="reason">Reason</label>
+      <input id="reason" v-model="evidence.status.reason" />
+    </div>
+    <button type="submit">Create Evidence</button>
+  </form>
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 import PageHeader from '@/components/PageHeader.vue';
 import PageSubHeader from '@/components/PageSubHeader.vue';
-import type { Activity } from '@/stores/activities.ts';
-import { type Evidence, useEvidenceStore } from '@/stores/evidence.ts';
+import { useEvidenceStore, type Evidence } from '@/stores/evidence.ts';
+import router from '@/router';
 
 const evidenceStore = useEvidenceStore();
+const evidence = ref<Partial<Evidence>>({
+  status: {
+    state: '',
+    reason: '',
+  },
+});
+const error = ref<string | null>(null);
+const success = ref<boolean>(false);
 
-const evidence = ref<Evidence>({} as Evidence);
-const activities = ref<Activity[]>([] as Activity[]);
-
-onMounted(() => {});
+async function submit() {
+  const response = await evidenceStore.create(evidence.value);
+  evidence.value = response.data;
+  router.push({ name: 'evidence:view', params: { id: response.data.id } });
+}
 </script>
