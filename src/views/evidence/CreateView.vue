fcba6c87ad1f5d2ccdb56ac1dc02c083d16568c2
Chris <chris@linguine.tech>
2025-08-06 13:03:30 +0100

    Add label fieds

--- src/views/evidence/CreateView.vue
+++ src/views/evidence/CreateView.vue
@@ -31,11 +31,32 @@
     </div>
     <div>
       <label for="status">Status</label>
-      <input id="status" v-model="evidence.status.state" required />
+      {{ status }}
+      <SelectButton
+        v-model="status.state"
+        :options="['satisfied', 'not-satisfied', 'in-progress']"
+        required
+      />
     </div>
     <div>
       <label for="reason">Reason</label>
-      <input id="reason" v-model="evidence.status.reason" />
+      <InputText id="reason" v-model="status.reason" />
+    </div>
+    <div>
+      <p>Labels</p>
+      <button type="button" @click="labels.push({})" class="border">Add</button>
+      <div
+        v-for="(label, index) in labels"
+        :key="index"
+        class="mb-2 flex items-center justify-start"
+      >
+        <InputText v-model="label.name" placeholder="Label name" class="mb-2" />
+        <InputText
+          v-model="label.value"
+          placeholder="Label value"
+          class="mb-2"
+        />
+      </div>
     </div>
     <button type="submit">Create Evidence</button>
   </form>
@@ -44,11 +65,17 @@
 import { ref } from 'vue';
 import PageHeader from '@/components/PageHeader.vue';
 import PageSubHeader from '@/components/PageSubHeader.vue';
-import { useEvidenceStore, type Evidence } from '@/stores/evidence.ts';
+import {
+  useEvidenceStore,
+  type Evidence,
+  type EvidenceLabel,
+  type EvidenceStatus,
+} from '@/stores/evidence.ts';
 import router from '@/router';
 import DatePicker from '@/volt/DatePicker.vue';
 import Textarea from '@/volt/Textarea.vue';
 import InputText from '@/volt/InputText.vue';
+import SelectButton from '@/volt/SelectButton.vue';
 
 const evidenceStore = useEvidenceStore();
 const evidence = ref<Partial<Evidence>>({
@@ -57,6 +84,11 @@ const evidence = ref<Partial<Evidence>>({
     reason: '',
   },
 });
+const status = ref<EvidenceStatus>({
+  state: '',
+  reason: '',
+});
+const labels = ref<EvidenceLabel[]>([]);
 const error = ref<string | null>(null);
 const success = ref<boolean>(false);
 
