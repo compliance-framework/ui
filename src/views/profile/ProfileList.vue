7fa7070dab41eb6ce676d8a6c77f91304c3f2145
Sam Sherar <sam.sherar@container-solutions.com>
2025-08-11 16:27:48 +0100

    feat: change profile list to composables

--- src/views/profile/ProfileList.vue
+++ src/views/profile/ProfileList.vue
@@ -4,7 +4,17 @@
     class="mt-4 rounded-md bg-white dark:bg-slate-900 border border-ccf-300 dark:border-slate-700"
   >
     <table class="table-auto w-full rounded-full dark:text-slate-300 border-collapse">
-      <tbody>
+      <tbody v-if="isLoading">
+        <tr>
+          <td class="py-3 px-4 font-medium text-wrap">Loading ...</td>
+        </tr>
+      </tbody>
+      <tbody v-else-if="error">
+        <tr>
+          <td class="py-3 px-4 font-medium text-wrap text-red-500">Error loading profiles</td>
+        </tr>
+      </tbody>
+      <tbody v-else>
       <tr
         class="hover:bg-zinc-50 dark:hover:bg-slate-800 border-b border-ccf-300 dark:border-slate-800"
         v-for="profile in profiles"
@@ -26,19 +36,26 @@
 
 <script lang="ts" setup>
 import PageHeader from '@/components/PageHeader.vue';
-import { useProfileStore, type Profile } from '@/stores/profiles';
-import { onMounted, ref } from 'vue';
+import { type Profile } from '@/stores/profiles';
+import { useDataApi } from '@/composables/axios';
+import { watch } from 'vue';
+import { useToast } from 'primevue/usetoast';
+import type { AxiosError } from 'axios';
+import type { ErrorResponse, ErrorBody } from '@/stores/types';
 
-const profileStore = useProfileStore();
-const profiles = ref<Profile[]>([]);
+const toast = useToast();
 
-onMounted(() => {
-  profileStore.list().then(resp => {
-    profiles.value = resp.data;
-  }).catch(async (response) => {
-    const error = await response.json();
-    console.error('Error fetching profiles:', error);
-  });
+const { data: profiles, isLoading, error } = useDataApi<Profile[]>('/api/oscal/profiles');
+
+watch(error, () => {
+  if (error.value) {
+    const axiosError = error.value as AxiosError<ErrorResponse<ErrorBody>>;
+    toast.add({
+      severity: 'error',
+      summary: 'Error loading profiles',
+      detail: axiosError.response?.data.errors.body || 'Unknown error',
+      life: 3000,
+    });
+  }
 });
-
-  </script>
+</script>
