de44aad734cedececd96b9d5139c45a10b01a491
Sam Sherar <sam.sherar@container-solutions.com>
2025-08-12 13:00:49 +0100

    feat: update roles to composables

--- src/views/RoleListView.vue
+++ src/views/RoleListView.vue
@@ -5,7 +5,13 @@
   <div
     class="mt-4 rounded-md bg-white dark:bg-slate-900 border-collapse border border-ccf-300 dark:border-slate-700"
   >
-    <CollapsableGroup v-for="role in roles" :key="role.id">
+  <template v-if="isLoading">
+      <p aria-live="polite" role="status">Loading...</p>
+    </template>
+    <template v-if="error">
+      <p class="text-red-500" aria-live="assertive" role="alert">Error loading roles</p>
+    </template>
+    <CollapsableGroup v-else v-for="role in roles" :key="role.id">
       <template #header>
         <div class="py-2 px-4 flex items-center gap-4">
           <h3>
@@ -14,24 +20,17 @@
         </div>
       </template>
       <div class="px-4 py-4 dark:bg-slate-950 border-b dark:border-slate-700">
-        <p>{{ role.description}}</p>
+        <p>{{ role.description }}</p>
       </div>
     </CollapsableGroup>
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
 import PageHeader from '@/components/PageHeader.vue'
 import PageSubHeader from '@/components/PageSubHeader.vue'
 import CollapsableGroup from '@/components/CollapsableGroup.vue'
-import { type Role, useRoleStore } from '@/stores/roles.ts'
+import { type Role } from '@/stores/roles.ts'
+import { useDataApi } from '@/composables/axios';
 
-const roleStore = useRoleStore()
-const roles = ref<Role[]>([] as Role[]);
-
-onMounted(() => {
-  roleStore.list().then((data) => {
-    roles.value = data.data
-  })
-})
+const { data: roles, isLoading, error } = useDataApi<Role[]>('/api/oscal/roles');
 </script>
