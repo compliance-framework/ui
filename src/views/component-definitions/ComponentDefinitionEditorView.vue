342658e997f5303237426e17f5c48bc36c92a785
Chris <chris@linguine.tech>
2025-09-16 11:20:18 +0100

    Remove unused and incorrect implementation

--- src/views/component-definitions/ComponentDefinitionEditorView.vue
+++ src/views/component-definitions/ComponentDefinitionEditorView.vue
@@ -29,15 +29,6 @@
       <RouterLink
         class="px-4 py-2 inline-block text-lg border-ccf-300 dark:border-slate-700 dark:hover:bg-slate-900"
         :to="{
-          name: 'component-definition-import-definitions',
-          params: { id: componentDefinition.uuid },
-        }"
-      >
-        Import Definitions
-      </RouterLink>
-      <RouterLink
-        class="px-4 py-2 inline-block text-lg border-ccf-300 dark:border-slate-700 dark:hover:bg-slate-900"
-        :to="{
           name: 'component-definition-components',
           params: { id: componentDefinition.uuid },
         }"
