153b495af1a352fb5e658b5dc7179d532d8b0f58
Chris <chris@linguine.tech>
2025-09-23 14:12:22 +0100

    Create and edit stes

--- src/views/workflows/partials/ActivityPanel.vue
+++ src/views/workflows/partials/ActivityPanel.vue
@@ -9,19 +9,18 @@ import Dialog from '@/volt/Dialog.vue';
 import ActivityEditForm from './ActivityEditForm.vue';
 import VueMarkdown from 'vue-markdown-render';
 
-import {
-  moveArrayElement,
-  useSortable,
-} from '@vueuse/integrations/useSortable';
-import { useTemplateRef, ref, nextTick, watch, toValue } from 'vue';
+import { useSortable } from '@vueuse/integrations/useSortable';
+import { useTemplateRef, ref, watch, toValue } from 'vue';
 import { useActivityStore } from '@/stores/activities';
 import { useToast } from 'primevue/usetoast';
+import StepEditForm from './StepEditForm.vue';
+import Button from '@/volt/Button.vue';
+import { BIconPencil } from 'bootstrap-icons-vue';
+import StepCreateForm from './StepCreateForm.vue';
 
-const {
-  value: editing,
-  toggle: toggleEditing,
-  set: setEditing,
-} = useToggle(false);
+const { value: editingActivity, set: setEditingActivity } = useToggle(false);
+const { value: editingStep, set: setEditingStep } = useToggle(false);
+const { value: creatingStep, set: setCreatingStep } = useToggle(false);
 const activityStore = useActivityStore();
 const toast = useToast();
 
@@ -35,6 +34,7 @@ const emit = defineEmits<{
 }>();
 
 const stepList = ref<Step[]>(props.activity?.steps || []);
+const currentStep = ref<Step>({} as Step);
 
 const steps = useTemplateRef<HTMLElement>('steps');
 useSortable(steps, stepList, {
@@ -48,13 +48,44 @@ watch(stepList, () => {
   });
 });
 
+async function onStepUpdated(step: Step) {
+  const activity = {
+    ...props.activity,
+    steps: stepList.value.map((i) => {
+      if (i.uuid == step.uuid) {
+        return step;
+      }
+      return i;
+    }),
+  };
+  await updateActivity(activity);
+  emit('updated', activity);
+  setEditingStep(false);
+}
+
+async function onStepCreated(step: Step) {
+  stepList.value.push(step);
+  const activity = {
+    ...props.activity,
+    steps: stepList.value,
+  };
+  await updateActivity(activity);
+  emit('updated', activity);
+  setCreatingStep(false);
+}
+
 async function onActivityUpdated(activity: Activity) {
   emit('updated', activity);
-  setEditing(false);
+  setEditingActivity(false);
 }
 
 async function remove() {
   emit('remove', props.activity);
+}
+
+function editStep(step: Step) {
+  currentStep.value = step;
+  setEditingStep(true);
 }
 
 async function updateActivity(activity: Activity): Promise<void> {
@@ -89,7 +120,7 @@ async function updateActivity(activity: Activity): Promise<void> {
         <!-- <Badge value="AC-1" severity="info" /> -->
         <div class="flex gap-2">
           <button
-            @click="toggleEditing"
+            @click="setEditingActivity(true)"
             class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
           >
             Edit
@@ -127,18 +158,56 @@ async function updateActivity(activity: Activity): Promise<void> {
         ref="steps"
       >
         <template #content="slotProps">
-          <h2 class="font-medium">{{ slotProps.item.title }}</h2>
-          <p class="py-2">{{ slotProps.item.description }}</p>
+          <div class="flex items-center">
+            <h5>{{ slotProps.item.title }}</h5>
+            <Button
+              variant="text"
+              size="small"
+              @click="editStep(slotProps.item)"
+              ><BIconPencil
+            /></Button>
+          </div>
+          <p class="py-2 fonnt-light">{{ slotProps.item.description }}</p>
         </template>
       </Timeline>
+      <Button size="small" @click="setCreatingStep(true)">Add Step</Button>
     </div>
   </Panel>
 
-  <Dialog header="Edit Activity" size="lg" v-model:visible="editing" modal>
+  <Dialog
+    header="Edit Activity"
+    size="lg"
+    v-model:visible="editingActivity"
+    modal
+  >
     <ActivityEditForm
       @updated="onActivityUpdated"
-      @cancel="toggleEditing"
+      @cancel="setEditingActivity(false)"
       :activity="activity"
+    />
+  </Dialog>
+
+  <Dialog
+    header="Create Step"
+    size="lg"
+    v-model:visible="creatingStep"
+    modal
+    :draggable="false"
+  >
+    <StepCreateForm @submit="onStepCreated" @cancel="setCreatingStep(false)" />
+  </Dialog>
+
+  <Dialog
+    header="Edit Step"
+    size="lg"
+    v-model:visible="editingStep"
+    modal
+    :draggable="false"
+  >
+    <StepEditForm
+      @submit="onStepUpdated"
+      @cancel="setEditingStep(false)"
+      :step="currentStep"
     />
   </Dialog>
 </template>
---
+++ src/views/workflows/partials/StepEditForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <form @submit.prevent="emit('submit', step)">
+    <div class="mb-4">
+      <Label>UUID</Label>
+      <InputText v-model="step.uuid" disabled class="block w-full mt-1" />
+    </div>
+
+    <div class="mb-4">
+      <Label required>Title</Label>
+      <InputText v-model="step.title" required class="block w-full mt-1" />
+    </div>
+
+    <div class="mb-4">
+      <Label>Description</Label>
+      <Textarea
+        v-model="step.description"
+        class="block w-full mt-1 field-sizing-content"
+      />
+    </div>
+
+    <div class="mb-4">
+      <Label>Remarks</Label>
+      <Textarea
+        v-model="step.remarks"
+        class="block w-full mt-1 field-sizing-content"
+      />
+    </div>
+
+    <div class="flex gap-2">
+      <PrimaryButton type="submit">Save</PrimaryButton>
+      <SecondaryButton type="button" @click="emit('cancel')"
+        >Cancel</SecondaryButton
+      >
+    </div>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Step } from '@/oscal';
+import PrimaryButton from '@/components/PrimaryButton.vue';
+import SecondaryButton from '@/components/SecondaryButton.vue';
+import Label from '@/volt/Label.vue';
+import InputText from '@/volt/InputText.vue';
+import Textarea from '@/volt/Textarea.vue';
+
+const props = defineProps<{
+  step: Step;
+}>();
+
+const emit = defineEmits<{
+  submit: [step: Step];
+  cancel: [];
+}>();
+
+const step = ref(props.step);
+</script>
